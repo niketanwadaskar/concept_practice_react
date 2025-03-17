d8b48f3f07212fb38f6fe001ad3b42c8c1083d9d
Niketan <niketanw@mirakitech>
2025-03-17 11:52:36 +0530

    merging

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     <div className="App" id="model-root">
       {/* <HigherOrderComponentsPractice /> */}
       {/* {/* <UseCallBackHook /> */}
-      {/* <UseMemoHook /> */} */}
+      {/* <UseMemoHook /> */}
       {/* <ErrorBoundary /> */}
       {/* <CustomHook /> */}
       {/* <Redux /> */}
